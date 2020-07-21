846013ac446c60688f2a5dae98f6d7e9ade1ab23
Michael Telford <michael.telford@live.com>
2020-07-21 17:35:33 +0100

    Sorted out the index page

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,28 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="container mx-auto">
+    <div className='container mx-auto text-gray-900'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Tailwind CSS Portfolio</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className="text-center">
-        <h1 className="text-5xl font-semibold my-5">
-          Welcome to{' '}
-          <a href="https://nextjs.org" className="text-blue-600">
-            Next.js!
-          </a>
+      <main className='text-center'>
+        <h1 className='text-4xl font-semibold mt-5 mb-1'>
+          Tailwind Portfolio
         </h1>
-
-        <p className="description">
-          Get started by editing{' '}
-          <code className="font-mono bg-gray-200 p-1 rounded">
-            pages/index.js
-          </code>
+        <header className='text-center mb-5'>
+          <small>
+            Designed by {' '}
+            <a className='underline' href='https://github.com/michaeltelford'>Michael Telford</a>
+          </small>
+        </header>
+        <p className='mb-4'>
+          Click on a link to view the styled component:
         </p>
+        <ul className='underline'>
+          <li><a href='/shopify'>Shopify Registration</a></li>
+        </ul>
       </main>
     </div>
   );
---
+++ pages/shopify.js
@@ -0,0 +1,3 @@
+export default function Shopify() {
+  return <p>Shopify</p>;
+}
